5cab9fec310816d0a5fd8d224cc3d6285c311448
MatheusTurra <matheusfr.318@gmail.com>
2021-04-08 02:05:41 -0300

    Test error fixed

--- test/spec/ContatoControllerSpec.js
+++ test/spec/ContatoControllerSpec.js
@@ -1,5 +1,19 @@
-describe("ContatoController", () => {
-    it("Deve criar um Contato vazio quando nenhum parâmetro de rota for passado", () => {
-        expect($scope.contato._id).toBeUndefined();
-    });
+describe("ContatoController", function() {
+	var $scope;
+
+	beforeEach(function() {
+		module('contactList');
+		inject(function($injector){
+			$scope = $injector.get('$rootScope').$new();
+		});
+	});
+
+	it("Deve criar um Contato vazio quando nenhum parâmetro de rota for passado", 
+		inject(function($controller) {
+			$controller('ContatoController', {
+				'$scope': $scope
+			});
+			expect($scope.contato._id).toBeUndefined();
+	}));
+
 });
