32a806a0479c62b1fa05ec12f55f796f18c06d9e
MatheusTurra <matheusfr.318@gmail.com>
2021-04-16 00:17:18 -0300

    Travis test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require("./config/express")();
 require("./config/database")("mongodb://localhost/contactlist");
 
 app.listen(app.get("port"), () => {
-    console.log("Servidor rodando em http://localhost:" + app.get("port"));
+    console.log("Servidor rodando em -> http://localhost:" + app.get("port"));
 });
